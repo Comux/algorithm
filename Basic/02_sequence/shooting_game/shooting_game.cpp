5d8976e890898e5e9538e45dcbfb0723f0bef986
xjliang <242777152@qq.com>
2019-02-10 09:24:14 +0800

    rename src file and transform code style

--- Basic/02_sequence/shooting_game/shooting_game.cpp
+++ Basic/02_sequence/shooting_game/shooting_game.cpp
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #define N 3003
 
@@ -7,30 +6,33 @@ int key[N];
 
 int read()
 {
-	int data = 0;
-	char ch = 0;
-	while (ch<'0'||ch>'9') ch=getchar();
-	while (ch>='0'&&ch<='9') data=data*10+ch-'0', ch=getchar();
-	return data;
+    int data = 0;
+    char ch = 0;
+    while (ch<'0'||ch>'9') ch=getchar();
+    while (ch>='0'&&ch<='9') data=data*10+ch-'0', ch=getchar();
+    return data;
 }
 
 void JosePhus(int n)
 {
     int i, k, m = 1;
     for (i = 0; i < n-1; i++)
+    {
         id[i] = i+1;
+    }
     
     k = n - 1;
     id[k] = 0;
 
     for (i = 1; i < n; i++)
     {
-		while (m-- > 1) {
+        while (m-- > 1)
+        {
             k = id[k];
         }
         m = key[ id[k] ];
         printf("%d ", id[k]+1);
-        id[k] = id[ id[k] ];
+        id[k] = id[id[k]];
     }
     printf("%d", id[k]+1);
 }
@@ -40,7 +42,9 @@ int main(void)
     int n, i;
     scanf("%d", &n);
     for (i = 0; i < n; i++)
-    	key[i] = read();
+    {
+        key[i] = read();
+    }
     JosePhus(n);
 
     return 0;
