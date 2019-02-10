24eb17f15e7f5f09a7b949f32f2c87a0197aa68b
xjliang <242777152@qq.com>
2019-02-10 09:40:22 +0800

    rename a.cpp to range.cpp and transform tab to spaces, modified some code styles

--- Basic/05_sort/range/range.cpp
+++ Basic/05_sort/range/range.cpp
@@ -8,24 +8,25 @@ int main()
 {
     int n;
     scanf("%d", &n);
-    for (int j = 0; j < n; j++ ) {
+    for (int j = 0; j < n; j++ )
+    {
         scanf("%d %d", &in[j], &out[j]);
     }
 
     std::sort(in, in + n);
     std::sort(out, out + n);
 
-	int lastDelta = 0, lastStart = -1;;
+    int lastDelta = 0, lastStart = -1;;
     int inI = 0, outI = 0;
     int ans = 0, t1 = 1, t2 = 1;
     int delta = 0;
 
-	for (int t = in[0]; t <= out[n-1]; t++)
+    for (int t = in[0]; t <= out[n-1]; t++)
     {
         for(; inI < n && in[inI] <= t; inI++) { }
         for(; outI < n && out[outI] <= t; outI++) { }
 
-		delta = inI - outI;
+        delta = inI - outI;
         if (delta != lastDelta)
         {
             if (ans < lastDelta)
@@ -38,6 +39,7 @@ int main()
             lastDelta = delta;
         }
     }
+
     printf("%d-%d %d", t1, t2, ans);
     return 0;
 }
